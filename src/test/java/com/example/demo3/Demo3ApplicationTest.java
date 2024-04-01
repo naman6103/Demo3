9e701891f974be31c62f2b930b72035aa6b6429f
NAMAN JAIN <naman.jain1@tothenew.com>
2024-04-01 15:20:30 +0530

    Jacoco commit 4

--- src/test/java/com/example/demo3/Demo3ApplicationTest.java
+++ src/test/java/com/example/demo3/Demo3ApplicationTest.java
@@ -1,9 +1,13 @@
 package com.example.demo3;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@SpringBootTest
+@RunWith( MockitoJUnitRunner.class )
 class Demo3ApplicationTest {
 
 	@Test
