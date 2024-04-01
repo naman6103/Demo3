08647e8d01907159ac4f30c02448c1e0a4315e78
NAMAN JAIN <naman.jain1@tothenew.com>
2024-04-01 15:48:49 +0530

    Jacoco commit 5

---
+++ src/main/java/com/example/demo3/Calc.java
@@ -0,0 +1,10 @@
+package com.example.demo3;
+
+public class Calc {
+
+    public Integer divide(int a, int b) {
+        if (b == 0) {
+            return null;
+        } else return a / b;
+    }
+}
